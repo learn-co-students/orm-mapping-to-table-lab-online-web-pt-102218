19ffe7a7b5eb138120a9ed5f99b85ebfc4c3e48f
Dakota Clark <bannaray95@gmail.com>
2019-02-05 00:58:28 +0000

    Done.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,71 +1,60 @@
-require "spec_helper"
+require_relative 'spec_helper'
 
-describe "Student" do
-  let(:josh) {Student.new("Josh", "9th")}
+describe Student do
 
-  before(:each) do
-    DB[:conn].execute("DROP TABLE IF EXISTS students")
+  before do
+    Student.create_table
   end
 
-  describe "when initialized with a name and a grade" do
-    it 'the name attribute can be accessed' do
-      student = Student.new("Tiffany", "11th")
-      expect(student.name).to eq("Tiffany")
+  after do
+    Student.drop_table
+  end
+
+  let(:pat) {Student.new}
+  let(:attributes) {
+    {
+      :id => 1,
+      :name => 'Pat',
+      :grade => 12
+    }
+  }
+
+  describe 'attributes' do 
+    it 'has an id, name, grade' do
+      pat.id = attributes[:id]
+      pat.name = attributes[:name]
+      pat.grade = attributes[:grade]
+
+      expect(pat.id).to eq(attributes[:id])
+      expect(pat.name).to eq(attributes[:name])
+      expect(pat.grade).to eq(attributes[:grade])
     end
-
-    it 'the grade attribute can be accessed' do
-      student = Student.new("Tiffany", "11th")
-      expect(student.grade).to eq("11th")
-    end
   end
 
-  it 'responds to a getter for :id' do
-    expect(josh).to respond_to(:id)
-  end
-
-  it 'does not provide a setter for :id' do
-    expect{josh.id = 1}.to raise_error(NoMethodError)
-  end
-
-  describe ".create_table" do
-    it 'creates the students table in the database' do
+  describe '.create_table' do
+    it 'creates a student table' do
+      Student.drop_table
       Student.create_table
+
       table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='students';"
       expect(DB[:conn].execute(table_check_sql)[0]).to eq(['students'])
     end
   end
 
-  describe ".drop_table" do
-    it 'drops the students table from the database' do
+  describe '.drop_table' do
+    it "drops the student table" do
       Student.create_table
       Student.drop_table
+
       table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='students';"
-      expect(DB[:conn].execute(table_check_sql)[0]).to eq(nil)
+      expect(DB[:conn].execute(table_check_sql)[0]).to be_nil
     end
   end
 
-  describe "#save" do
-    it 'saves an instance of the Student class to the database' do
-      Student.create_table
-      josh.save
-      expect(josh.id).to eq(1)
-      expect(DB[:conn].execute("SELECT * FROM students")).to eq([[1, "Josh", "9th"]])
-    end
-  end
-
-  describe ".create" do
-    before(:each) do
-      Student.create_table
-    end
-    it 'takes in a hash of attributes and uses metaprogramming to create a new student object. Then it uses the #save method to save that student to the database' do
-      Student.create(name: "Sally", grade: "10th")
-      expect(DB[:conn].execute("SELECT * FROM students")).to eq([[1, "Sally", "10th"]])
-    end
-    it 'returns the new object that it instantiated' do
-      student = Student.create(name: "Josh", grade: "9th")
-      expect(student).to be_a(Student)
-      expect(student.name).to eq("Josh")
-      expect(student.grade).to eq("9th")
+  describe "#save" do 
+    it 'saves an instance of the Student class to the database' do 
+      pat.save
+      expect(DB[:conn].execute("SELECT * FROM students")).to eq([[1, nil, nil]])
     end
   end
 end
